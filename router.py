0a43ec0e60034f19b0b28d038c72581eb6598fd5
Ted Benson <edward.benson@gmail.com>
2014-01-22 13:49:54 -0500

    Wire in the listener

--- router.py
+++ router.py
@@ -1,18 +1,21 @@
 from persistence import SimpleFilePersistence as Persistence
 from parser import parse_tweet
+from listener import Listener
 
 class Router:
   def __init__(self, config):
     self.config = config
 
-  def start():
+  def start(self):
     self.persistence= Persistence(self.config)
     self.persistence.open()
+    self.listener = Listener(self.config, self.handleNewTweet)
   
-  def stop():
+  def stop(self):
     self.persistence.close()
 
-  def handleNewTweet(tweet):
+  def handleNewTweet(self, tweet):
+    """This gets called by the listener when there is a new tweet to record."""
     data = parse_tweet(tweet)
     persistence.record(data)
 
