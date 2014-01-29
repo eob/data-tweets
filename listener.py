bca4cb3047bf4fe1dbc8f7dc0a75b025d60113ca
Ted Benson <edward.benson@gmail.com>
2014-01-29 09:54:53 -0500

    changes

--- listener.py
+++ listener.py
@@ -2,8 +2,17 @@ class Listener:
   """
 
   """
-  def __init__(self, config, sinkFn):
+  def __init__(self, config, sinkFn, twitter):
     self.config = config
     self.sinkFn = sinkFn
+    self.twitter = twitter
+
+  def fetch_direct_messages(self):
+     statuses = self._api.GetDirectMessages(page=1)
+     print statuses
+
+  def start(self):
+    pass
+
 
 
--- persistence.py
+++ persistence.py
@@ -1,9 +1,29 @@
 import json
+import tempfile, shutil, os
+import uuid
 
 class SimpleFilePersistence:
-  def __init__(self, config):
-    self.config = config
-    self.fname = config.get('Logging', 'filename', 0)
+  def __init__(self, my_config, global_config):
+    self.my_config = my_config
+    self.global_config = global_config
+
+    # Ensure output directory
+    d = os.path.dirname(global_config['persistence']['output_dir'])
+    if not os.path.exists(d):
+      print "Creating directory " + global_config['persistence']['output_dir']
+      os.makedirs(d)
+    self.fname = os.path.join(global_config['persistence']['output_dir'],
+        my_config['name'] + '.dat')
+
+  def tweets(self):
+    temp_dir = tempfile.gettempdir()
+    temp_path = os.path.join(temp_dir, str(uuid.uuid4()))
+    shutil.copy2(self.fname, temp_path)
+    f = open(temp_path, 'r')
+    for line in f:
+      row = json.parse(line)
+      yield row
+    f.close()
 
   def open(self):
     self.fout = open(self.fname, 'w')
