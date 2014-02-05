9a01dd3d12c170faa10a9a537cb72782994ec332
Ted Benson <edward.benson@gmail.com>
2014-02-05 17:36:16 -0500

    example

---
+++ server/config/example_secrets.js
@@ -0,0 +1,68 @@
+module.exports = {
+  db: 'localhost',
+
+  sessionSecret: "Your Session Secret goes here",
+
+  sendgrid: {
+    user: 'Your SendGrid Username',
+    password: 'Your SendGrid Password'
+  },
+
+  nyt: {
+    key: 'Your New York Times API Key'
+  },
+
+  lastfm: {
+    api_key: 'Your API Key',
+    secret: 'Your API Secret'
+  },
+
+  facebook: {
+    clientID: 'Your App ID',
+    clientSecret: 'Your App Secret',
+    callbackURL: '/auth/facebook/callback',
+    passReqToCallback: true
+  },
+
+  github: {
+    clientID: 'Your Client ID',
+    clientSecret: 'Your Client Secret',
+    callbackURL: '/auth/github/callback',
+    passReqToCallback: true
+  },
+
+  twitter: {
+    consumerKey: '',
+    consumerSecret: '',
+    callbackURL: 'http://box.csail.mit.edu/auth/twitter/callback',
+    userAuthorizationURL: 'https://api.twitter.com/oauth/authorize',
+    passReqToCallback: true
+  },
+
+  google: {
+    clientID: '',
+    clientSecret: '',
+    callbackURL: '/auth/google/callback',
+    passReqToCallback: true
+  },
+
+  tumblr: {
+    consumerKey: 'Your Consumer Key',
+    consumerSecret: 'Your Consumer Secret',
+    callbackURL: '/auth/tumblr/callback'
+  },
+
+  foursquare: {
+    clientId: 'Your Client ID',
+    clientSecret: 'Your Client Secret',
+    redirectUrl: 'http://localhost:3000/auth/foursquare/callback'
+  },
+
+  paypal: {
+    host: 'api.sandbox.paypal.com', // or api.paypal.com
+    client_id: 'Your Client ID',
+    client_secret: 'Your Client Secret',
+    returnUrl: 'http://localhost:3000/api/paypal/success',
+    cancelUrl: 'http://localhost:3000/api/paypal/cancel'
+  }
+};
